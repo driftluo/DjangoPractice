1cefcf3843d906328bdf6734f728b100c772e6ed
piaoliu <441594700@qq.com>
2017-02-07 23:16:48 +0800

    database timezone= asia/shanghai

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,6 +77,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TIME_ZONE': 'Asia/Shanghai',
     }
 }
 
