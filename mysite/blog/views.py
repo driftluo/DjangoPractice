f707638a5ed94b4c90daa149911d616900dbc14b
piaoliu <441594700@qq.com>
2017-02-10 01:47:26 +0800

    the blog index page url/view/html

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import ArticlePost, Comment
+
+def index(request):
+	post = ArticlePost.objects.filter(status='published')
+	return render(request, 'blog/list.html', {'posts': post})
