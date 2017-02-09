a2844505d079c25ff6e97945c3a0ef45e6b3c7e7
piaoliu <441594700@qq.com>
2017-02-09 17:29:34 +0800

    register admin site and customer it

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import ArticlePost, ArticleTag
 
-# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+	list_display = ('title', 'status', 'published')
+	list_filter = ('status', 'published')
+	search_fields = ('title', 'body')
+	ordering = ('status', 'published')
+	fieldsets = (
+		(None, {'fields': ['title', 'slug', 'author', 'body', 'status']}),
+		('Tag', {'fields': ['article_tag']}),
+		('Date information', {'fields': ['published']})
+	)		
+admin.site.register(ArticlePost, ArticleAdmin)
+admin.site.register(ArticleTag)
