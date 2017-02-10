3ffcdc5e78f81e27a4cb2d0dc13ad285b8ab7ca6
piaoliu <441594700@qq.com>
2017-02-10 20:56:07 +0800

    add comment in detail.html

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import ArticlePost, Comment
+from .forms import CommentForm
 
 def index(request):
 	post = ArticlePost.objects.filter(status='published')
@@ -9,4 +12,16 @@ def index(request):
 def article_detail(request, article_id):
 	article = get_object_or_404(ArticlePost, id=article_id)
 	comments = article.comments.filter(active=True)
-	return render(request, 'blog/detail.html', {'article':article, 'comments':comments})
+
+	if request.method == 'GET':
+		comment_form = CommentForm()
+
+	if request.method == 'POST':
+		comment_form = CommentForm(data=request.POST)
+		if comment_form.is_valid():
+			new_comment = comment_form.save(commit=False)
+			new_comment.articlepost = article
+			new_comment.save()
+			return HttpResponseRedirect(reverse('blog:article_detail', args=(article.id,)))
+
+	return render(request, 'blog/detail.html', {'article':article, 'comments':comments, 'comment_form':comment_form})
