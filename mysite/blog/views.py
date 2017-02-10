ad924fa4f659bf56e67f99dbb72e2d73682359f9
piaoliu <441594700@qq.com>
2017-02-10 23:26:56 +0800

    paginated view/html

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import ArticlePost, Comment
 from .forms import CommentForm
 
 def index(request):
 	post = ArticlePost.objects.filter(status='published')
-	return render(request, 'blog/list.html', {'posts': post})
+	paginator = Paginator(post, 15)  #每页15个标题
+	
+	page = request.GET.get('page')
+	try:
+		current_page = paginator.page(page)
+		post = current_page.object_list
+	except PageNotAnInteger:
+		current_page = paginator.page(1)
+		post = current_page.object_list
+	except EmptyPage:
+		current_page = paginator.page(paginator.num_pages)
+		post = current_page.object_list
+	return render(request, 'blog/list.html', {'posts': post, 'page':current_page})
 
 def article_detail(request, article_id):
 	article = get_object_or_404(ArticlePost, id=article_id)
